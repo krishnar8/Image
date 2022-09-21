690336e6e923a8403dc0a38f86e1fa8e9ce8e701
Gopi <krishnar8@gmail.com>
2022-09-21 20:45:48 +0530

    Updates

---
+++ precache-manifest.114e29a82cf312e049b10f19634bd017.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2942e7dac5290daf2e08959df0444c92",
+    "url": "/Image/index.html"
+  },
+  {
+    "revision": "85ab7b871055307da142",
+    "url": "/Image/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "05f51926edea0a705edc",
+    "url": "/Image/static/js/2.c4b25ccd.chunk.js"
+  },
+  {
+    "revision": "83d508e65579b4b814929af9a1619e62",
+    "url": "/Image/static/js/2.c4b25ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ab7b871055307da142",
+    "url": "/Image/static/js/main.55613cfd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Image/static/js/main.55613cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716d54ad1f69ae154d98",
+    "url": "/Image/static/js/runtime-main.3cde71b6.js"
+  }
+]);
